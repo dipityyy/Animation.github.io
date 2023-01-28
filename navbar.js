bc7f608a9b0ec7e6b22dfa40455f01d910389835
Sari Broudy <106028585+dipityyy@users.noreply.github.com>
2023-01-28 15:51:28 -0500

    Update navbar.js

--- navbar.js
+++ navbar.js
@@ -2,5 +2,5 @@ function toggleNavbar(collapseID) {
   document.getElementByID(collapseID).classlist.toggle("hidden");
   document.getElementByID(collapseID).classlist.toggle("block");
 }
-alert('stop')
+
   
