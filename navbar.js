6edda9a9c8bb134ef18639ca785932a81cd03f56
Sari Broudy <106028585+dipityyy@users.noreply.github.com>
2023-01-28 15:46:28 -0500

    Create navbar.js

---
+++ navbar.js
@@ -0,0 +1,6 @@
+function toggleNavbar(collapseID) {
+  document.getElementByID(collapseID).classlist.toggle("hidden");
+  document.getElementByID(collapseID).classlist.toggle("block");
+}
+alert('stop')
+  
